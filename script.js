4ff26d9ac5ef6d660b1ae0e36447a3a32ef7e9fc
SHINDY BABA <shindyhh@gmail.com>
2023-08-13 15:34:42 +0900

    implemented active nav link

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// pop up
+
 const popup = document.getElementById("popup");
 
 function openPopup() {
@@ -8,8 +10,29 @@ function closePopup() {
   popup.classList.remove("open-popup");
 }
 
-
+// sticky navbar on scroll
 window.addEventListener("scroll", function() {
   const header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
 })
+
+// active nav link
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop -150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*="' + id + '"]').classList.add('active');
+      })
+    }
+  })
+}
