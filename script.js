a39c023c7e2e8eeb893a1ab312a3711d8b1ae985
SHINDY BABA <shindyhh@gmail.com>
2023-12-09 11:06:55 +0900

    prettier changes js

--- script.js
+++ script.js
@@ -11,32 +11,33 @@ function closePopup() {
 }
 
 // sticky navbar on scroll
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function () {
   const header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
-})
+});
 
 // active nav link
 
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
-  sections.forEach(sec => {
+  sections.forEach((sec) => {
     let top = window.scrollY;
-    let offset = sec.offsetTop -150;
+    let offset = sec.offsetTop - 150;
     let height = sec.offsetHeight;
-    let id = sec.getAttribute('id');
+    let id = sec.getAttribute("id");
 
     if (top >= offset && top < offset + height) {
-      navLinks.forEach(links => {
-        links.classList.remove('active');
-        document.querySelector('header nav a[href*="' + id + '"]').classList.add('active');
-      })
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector('header nav a[href*="' + id + '"]')
+          .classList.add("active");
+      });
     }
-  })
-}
-
+  });
+};
 
 // make mobile navbar work
 
@@ -44,20 +45,20 @@ const mobileNavLinks = document.querySelectorAll(".navbar a");
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
 
-btnNavEl.addEventListener('click', function() {
+btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
 
-mobileNavLinks.forEach(link => {
-  link.addEventListener('click', function(event) {
+mobileNavLinks.forEach((link) => {
+  link.addEventListener("click", function (event) {
     event.preventDefault();
     headerEl.classList.remove("nav-open");
   });
 });
 
 // Smooth scrolling for navigation links
-navLinks.forEach(link => {
-  link.addEventListener('click', function(event) {
+navLinks.forEach((link) => {
+  link.addEventListener("click", function (event) {
     event.preventDefault();
     const targetId = link.getAttribute("href");
     const targetSection = document.querySelector(targetId);
@@ -65,7 +66,7 @@ navLinks.forEach(link => {
 
     window.scrollTo({
       top: targetOffset - 50,
-      behavior: "smooth"
+      behavior: "smooth",
     });
 
     headerEl.classList.remove("nav-open");
@@ -76,21 +77,21 @@ navLinks.forEach(link => {
 const homeLink = document.querySelector("#home-link");
 const portfolioLink = document.querySelector("#portfolio-link");
 
-homeLink.addEventListener('click', function(event) {
+homeLink.addEventListener("click", function (event) {
   event.preventDefault();
   window.scrollTo({
     top: 0,
-    behavior: "smooth"
+    behavior: "smooth",
   });
 });
 
-portfolioLink.addEventListener('click', function(event) {
+portfolioLink.addEventListener("click", function (event) {
   event.preventDefault();
   const targetSection = document.querySelector("#portfolio");
   const targetOffset = targetSection.offsetTop;
 
   window.scrollTo({
     top: targetOffset - 50,
-    behavior: "smooth"
+    behavior: "smooth",
   });
 });
